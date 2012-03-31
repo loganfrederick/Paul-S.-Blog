593d483a281b72190714eccd30c00703bce480d7
Paul S. Chun <paul@soloquartet.com>
2012-03-30 20:26:19 -0700

    Added more stuff.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#links li.first-level").hover(function() {
-	$(this).children("a:first").animate({"background-color": "#900"}, 200);
+	$(this).children("a:first").animate({"background-color": "#CA7"}, 200);
 	if($(this).children(".second-level").length > 0) {
 	    $(this).children(".second-level").fadeIn(200);
 	}
     }, function() {
-	$(this).children("a:first").animate({"background-color": "#600"}, 100);
+	$(this).children("a:first").animate({"background-color": "#B1946C"}, 100);
 	if($(this).children(".second-level").length > 0) {
 	    $(this).children(".second-level").fadeOut(200);
 	}
     });
     $("#links .second-level li").hover(function() {
-	$(this).children("a:first").css({"background-color": "#900"});
+	$(this).children("a:first").animate({"background-color": "#CA7"}, 200);
     }, function() {
-	$(this).children("a:first").css({"background-color": "#600"});
+
     });
 });
