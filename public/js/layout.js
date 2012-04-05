db21dfbd0d4a9f7596bef37d7e832b0296510b0d
Paul S. Chun <paul@sixofhearts.us>
2012-04-05 02:01:04 -0700

    Fixed dumb jquery flashing.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
     $("#links li.first-level, #logo").hover(function() {
-	$(this).children("a:first").animate({"background-color": "#900"}, 200);
+	$(this).children("a:first").animate({"background-color": "#990000"}, 200);
 	if($(this).children(".second-level").length > 0) {
 	    $(this).children(".second-level").fadeIn(200);
 	}
     }, function() {
-	$(this).children("a:first").animate({"background-color": "#600"}, 100);
+	$(this).children("a:first").animate({"background-color": "#660000"}, 100);
 	if($(this).children(".second-level").length > 0) {
 	    $(this).children(".second-level").fadeOut(200);
 	}
     });
     $("#links .second-level li").hover(function() {
-	$(this).children("a:first").animate({"background-color": "#900"}, 200);
+	$(this).children("a:first").animate({"background-color": "#990000"}, 200);
     }, function() {
-	$(this).children("a:first").animate({"background-color": "#600"}, 200);
+	$(this).children("a:first").animate({"background-color": "#660000"}, 200);
     });
 });
